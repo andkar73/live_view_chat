a53caf8f7dac4b8c81b179ca36f3f2b4e49177f0
Andreas Karlsson <andkar73@gmail.com>
2019-12-30 16:05:17 +0100

    first commit

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :live_view_chat,
+  ecto_repos: [LiveViewChat.Repo]
+
+# Configures the endpoint
+config :live_view_chat, LiveViewChatWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "TI1W22NfYCealIPamFU48+bQDw6eNwkt5VqZpFiin5cJ4h0U5Bq6kDP0bP4qC6Sn",
+  render_errors: [view: LiveViewChatWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: LiveViewChat.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [ signing_salt: "nboP/m96VTQaGdzANLVyJpoA7lSNvW9b"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
