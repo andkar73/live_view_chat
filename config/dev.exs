812022959a15c6fcb68334d176f3b14852b73ee9
Andreas Karlsson <andkar73@gmail.com>
2020-01-23 19:44:09 +0100

    cleaan up config dev

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :live_view_chat, LiveViewChat.Repo,
-  username: "vagrant",
-  password: "rasputin",
+  username: "",
+  password: "",
   database: "live_view_chat_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
