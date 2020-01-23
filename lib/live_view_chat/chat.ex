966208249067ba9c4cefb48dfb03eab68b8c0b74
Andreas Karlsson <andkar73@gmail.com>
2020-01-23 19:40:39 +0100

    a smal refactoring

--- lib/live_view_chat/chat.ex
+++ lib/live_view_chat/chat.ex
@@ -1,14 +1,31 @@
 defmodule LiveViewChat.Chat do
   alias LiveViewChat.Chat.ChatPost
-  alias  LiveViewChat.Repo
-  import Ecto.Query
-
+  alias LiveViewChatWeb.Presence
 
   @doc """
-  Create a changeset to change a chat_post
+  Create a changeset to change a chat_post.
+  Returns a changeset.
   """
   def change_chat_post(chat_post, changes \\%{}) do
     ChatPost.changeset(chat_post, changes)
+  end
+
+  @doc """
+  Create a random string containing letters and numbers.
+  Returns a string
+  """
+  def random_string do
+    alphabet = Enum.to_list(?a..?z) ++ Enum.to_list(?0..?9)
+    length = 12
+    for _ <- 1..length, into: "", do: << Enum.random(alphabet) >>
+  end
+
+  @doc """
+  Returns the number of user assigned to a topic.
+  """
+  def number_of_users(topic) do
+    %{"user" => %{metas: user_list}} = Presence.list(topic)
+    length(user_list)
   end
 
 end
--- lib/live_view_chat_web/live/chat/chat.ex
+++ lib/live_view_chat_web/live/chat/chat.ex
@@ -10,37 +10,26 @@ defmodule LiveViewChatWeb.ChatLive.Chat do
     Presence.track(self(), @topic, "user", %{online_at: inspect(System.system_time(:second))})
     LiveViewChatWeb.Endpoint.subscribe(@topic)
     changeset = Chat.change_chat_post(%ChatPost{})
-    socket = assign(socket, changeset: changeset, chat_posts: [], number_of_users: number_of_users())
+    socket = assign(socket, changeset: changeset, chat_posts: [], number_of_users: Chat.number_of_users(@topic))
    {:ok, socket, temporary_assigns: [chat_posts: []]}
-  end
-
-  def handle_event("create", %{"chat_post" => chat_post}, socket) do
-    id = random_string()
-    LiveViewChatWeb.Endpoint.broadcast_from(self(), @topic, "create", %{chat_post: chat_post, id: id})
-    {:noreply, assign(socket, chat_posts: [chat_post], id: id)}
-  end
-
-  def handle_info(%{event: "presence_diff", payload: _payload}, socket) do
-    {:noreply, assign(socket, number_of_users: number_of_users())}
-  end
-
-  def handle_info(%{ event: "create", payload: %{chat_post: chat_post, id: id}}, socket) do
-    {:noreply, assign(socket, chat_posts: [chat_post], id: id)}
   end
 
   def render(assigns) do
     ChatView.render("chat.html", assigns)
   end
 
-  defp random_string do
-    alphabet = Enum.to_list(?a..?z) ++ Enum.to_list(?0..?9)
-    length = 12
-    for _ <- 1..length, into: "", do: << Enum.random(alphabet) >>
+  def handle_event("create", %{"chat_post" => chat_post}, socket) do
+    id = Chat.random_string()
+    LiveViewChatWeb.Endpoint.broadcast_from(self(), @topic, "create", %{chat_post: chat_post, id: id})
+    {:noreply, assign(socket, chat_posts: [chat_post], id: id)}
   end
 
-  defp number_of_users do
-    %{"user" => %{metas: user_list}} = Presence.list(@topic)
-    length(user_list)
+  def handle_info(%{event: "presence_diff", payload: _payload}, socket) do
+    {:noreply, assign(socket, number_of_users: Chat.number_of_users(@topic))}
+  end
+
+  def handle_info(%{ event: "create", payload: %{chat_post: chat_post, id: id}}, socket) do
+    {:noreply, assign(socket, chat_posts: [chat_post], id: id)}
   end
 
 end
